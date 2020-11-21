c2e24369f41afa80601dd60e3af68ba7b0e27071
julian.stein <julste99@web.de>
2020-11-21 14:11:39 +0100

    Initial Commit

---
+++ src/main/java/com/dhbw/btreebackend/BTreeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dhbw.btreebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BTreeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BTreeBackendApplication.class, args);
+    }
+
+}
